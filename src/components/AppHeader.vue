f45853d41c9f7aa30566d08d15cfa99d8ee0a964
alessandrorumi <rumi.alessandro21@gmail.com>
2024-01-08 15:06:34 +0100

    Add: Componente AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<template>
+  <header>
+    <div class="logo me-3">
+      <img src="../assets/yu-gi-oh-logo.svg" alt="">
+    </div>
+    <h1>Yu-Gi-Oh Api</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/mixins' as *;
+@use '../styles/partials/variables.scss' as *;
+
+header {
+  height: 60px;
+  @include vert-align-center;
+
+  .logo {
+    img {
+      width: 100px;
+    }
+  }
+}
+</style>
